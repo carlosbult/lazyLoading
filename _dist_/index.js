3f36993a0db97a06b5e9a5b39fce66c6cb34b8df
carlosbult <carlosbult@users.noreply.github.com>
2021-06-25 00:18:44 +0000

    deploy: fbe611f8887052eea32e58993fb8de962a3391c6

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,72 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage, loadedImg, img0} from './lazy.js'
 
-console.log('Happy hacking :)')
+let url = "https://randomfox.ca/floof/";
+
+const container = document.querySelector('#containerImg')
+
+const button = document.createElement('button');
+button.textContent = 'Obten un nuevo Zorro';
+button.className = 'boton';
+container.appendChild(button);
+
+const clean = document.createElement('button');
+clean.textContent = 'Limpiar';
+clean.className = 'clean';
+container.appendChild(clean);
+
+let imgContainer = document.createElement('div');
+container.appendChild(imgContainer);
+
+let imagen;
+
+let totalImg = 0;
+
+export let report = () => {
+    console.log(`
+    Imagenes cargadas ${totalImg}
+    Imagenes Vistas ${loadedImg()}
+    `);
+}
+
+async function fetchData() {
+    let response = await fetch(url);
+    let jsonConv = await response.json();
+    let urlImg = jsonConv.image;
+
+    imagen = document.createElement('img');
+    imagen.dataset.src = urlImg;
+    imagen.classList = 'image mx-auto'
+    imgContainer.appendChild(imagen);
+
+    totalImg++;
+
+    registerImage(imagen);
+    report();
+}
+
+function removeImg() {
+    const extractImg = container.children[2].childNodes;
+    const imgs = [...extractImg];
+    imgs.forEach((i) => i.remove())
+
+    console.clear();
+
+    totalImg = 0;
+
+    img0();
+}
+
+button.addEventListener('click', (e) => fetchData());
+clean.addEventListener('click', (e) => removeImg());
+
+
+
+
+
+
+
+
+
+
+
+
