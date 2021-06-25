fbe611f8887052eea32e58993fb8de962a3391c6
carlosbult <acevedo.bult@gmail.com>
2021-06-24 20:17:46 -0400

    Js files

---
+++ src/lazy.js
@@ -0,0 +1,36 @@
+import { report } from './index';
+
+const observer = new IntersectionObserver((entries) => {
+    entries.filter(isIntersecting).forEach(loadImage);
+});
+
+const isIntersecting = (entry) => {
+    return entry.isIntersecting
+}
+
+export const registerImage = (imagen) => {
+    observer.observe(imagen);
+}
+
+let img = 0
+
+export const loadImage = (entry) => {
+    const nodo = entry.target;
+    nodo.src = nodo.dataset.src;
+    img++;
+    report()
+    observer.unobserve(nodo);
+}
+
+export const loadedImg = () => {
+    return img;
+}
+
+export const img0 = () =>{
+    return img = 0;
+}
+
+
+
+
+
